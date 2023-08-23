0ebaca91a92d92b2c37235d5ea967eb090f59a17
Henry Philpotts <hphilpotts@gmail.com>
2023-08-23 14:54:35 +0100

    h2h page component created and added to Routes.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { TeamList } from './components/TeamList';
 import { Home } from './pages/Home'
 import { EnglandRound } from './pages/EnglandRound';
 import { StandingsRound } from './pages/StandingsRound';
+import { HeadToHeadRound } from './pages/HeadToHeadRound';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='/england' element={<EnglandRound />} />
               <Route path='/standings' element={<StandingsRound />} />
+              <Route path='/headtohead' element={<HeadToHeadRound />} />
             </Routes>
           </BrowserRouter>
       </ThemeProvider>
---
+++ src/pages/HeadToHeadRound.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const HeadToHeadRound = () => {
+  return (
+    <>
+        <h1>Head to Head</h1>
+    </>
+  )
+}
+
+/*
+
+QUESTIONS:
+
+True or false. There will be a super over.
+Pace or spin. What will take more wickets?
+Bat or chase. Captains will choose to do either more?
+Bat first or chase. Which will win more games?
+Who wins. India or Pakistan or Game cancelled 
+
+Consider using radio buttons or an even more preferable alternative like:
+-> https://mui.com/material-ui/react-toggle-button/
+
+Two components available:
+
+1. **RoundTitle Component**
+2. **QuestionTitle Component**
+
+The "Round" should be implemented as a <Card /> component. 
+Apply sx={{ my: 2 }} styling to it, similar to how it's done for the "EnglandRound" and "StandingsRound".
+
+*/
