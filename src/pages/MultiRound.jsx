9c84d2cbb1f5843f77dd68476eeb33b982b6d44e
miles <mdnrosen@hotmail.co.uk>
2023-09-05 19:10:00 +0100

    sixes round working, figured out best way to handle disabling of other options for same question

--- src/pages/MultiRound.jsx
+++ src/pages/MultiRound.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Box, Card, CardContent, FormControl, Grid, InputLabel, MenuItem, Select, Toolbar, Typography } from '@mui/material'
 import { RoundTitle } from '../components/RoundTitle'
@@ -6,16 +6,17 @@ import { QuestionTitle } from '../components/QuestionTitle'
 import { RoundCard } from '../components/RoundCard'
 import { useGuesses } from '../context/GuessContext'
 import squads from '../assets/data/squads.json'
+import { getOptions } from '../utils/helpers'
+
 
 export const MultiRound = () => {
-    const { guesses } = useGuesses()
-    const allPlayers = squads.reduce((acc, cv) => {
-        const { players } = cv
-       return {
-        ...acc,
-        
-       }
-    },[])
+    const { guesses, updateGuesses } = useGuesses()
+    const [ allPlayers, setAllPlayers ] = useState(squads.map(squad => squad.players).flat())    
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        updateGuesses(name, value)
+    }
 
   return (
     <RoundCard>
@@ -25,32 +26,53 @@ export const MultiRound = () => {
             marking="Marking different for each question"
         />
         <CardContent>
-            <Box component="form">
+            <Box component="form" onChange={handleChange}>
                 <Grid container spacing={2}>
-                    
                     <Grid item xs={12}>
-                        <FormControl fullWidth>
-                            <QuestionTitle 
-                                title="Bigus Hittus"
-                                num="19"
-                                question="Pick 2 players. +1 point for each six they hit."
-                                hint="Simples, right?"
-                            />
-                            <Grid item container>
-                                <Grid item xs={6}>
+                        <QuestionTitle 
+                            title="Bigus Hittus"
+                            num="19"
+                            question="Pick 2 players. +1 point for each six they hit."
+                            hint="In 2019, Eoin Morgan hit the most (22)."
+                        />
+                        <Grid item container spacing={2}>
+                            <Grid item xs={6}>
+                                <FormControl fullWidth>
                                     <Select
-                                        name="multi_sixes"
-                                        value={guesses['multi_sixes'] || ''}
-        
-
-                                    ></Select>
-                                </Grid>
+                                        onChange={handleChange}
+                                        placeholder="First choice"
+                                        name="multi_sixes_1"
+                                        value={guesses['multi_sixes_1'] || ''}
+                                    >
+                                {allPlayers && getOptions(allPlayers, 'name', 'name').map((opt, i) => (
+                                         <MenuItem 
+                                            key={i} 
+                                            value={opt.value} 
+                                            disabled={opt.value === guesses['multi_sixes_2']}
+                                        >{opt.label}</MenuItem>                                ))}
+                                    </Select>
+                                </FormControl>  
                             </Grid>
-
-
-                        </FormControl>
+                            <Grid item xs={6}>
+                                <FormControl fullWidth>
+                                    <Select
+                                        onChange={handleChange}
+                                        placeholder="Second choice"
+                                        name="multi_sixes_2"
+                                        value={guesses['multi_sixes_2'] || ''}
+                                    >
+                                        {allPlayers && getOptions(allPlayers, 'name', 'name').map((opt, i) => (
+                                            <MenuItem 
+                                                key={i} 
+                                                value={opt.value} 
+                                                disabled={opt.value === guesses['multi_sixes_1']}
+                                            >{opt.label}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
                     </Grid>
-
                 </Grid>
             </Box>
         </CardContent>
