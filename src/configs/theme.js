9fb3df742f3c080ad0e4a155b96838b2818cc76c
miles <mdnrosen@hotmail.co.uk>
2023-09-19 09:30:12 +0100

    css changes for mobile

--- src/configs/theme.js
+++ src/configs/theme.js
@@ -18,10 +18,19 @@ const theme = createTheme({
         }
     },
     typography: {
+        fontSize: '1.2rem',
+        '@media (min-width:600px)': {
+          fontSize: '1.5rem',
+        },
+        [theme.breakpoints.up('md')]: {
+          fontSize: '2rem',
+        },
         fontFamily: [
             'Source Code Pro', 'monospace'
-        ].join(',')
-    }
+        ].join(','),
+    },
+
+
 })
 
 export default theme;
