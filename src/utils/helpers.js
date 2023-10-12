c2298e35313ee06d3302879880bca3ef15ae24e3
Henry Philpotts <hphilpotts@gmail.com>
2023-10-12 18:00:52 +0100

    Added helper to filter allGuesses.json by round.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,3 +6,23 @@ export const getOptions = (array, labelKey, valueKey) => {
         }
     })
 }
+
+// will work (...?) for each round, not just the England round?
+export const filteredGuessesByRound = (allData, roundPrefix) => {
+    return allData.body.map(object => {
+        return {
+            name: object.name,
+            id: object.id,
+            guesses: filterGuessObject(object.guesses, roundPrefix)
+        }
+    })
+}
+
+const filterGuessObject = (object, prefix) => {
+    return Object.keys(object)
+        .filter(key => key.startsWith(prefix))
+        .reduce((newObject, key) => {
+            newObject[key] = object[key];
+            return newObject;
+        }, {});
+}
